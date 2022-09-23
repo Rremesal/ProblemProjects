f2434a0954bfe042dca1931b358292f743be838c
Rremesal <raul.remesal@gmail.com>
2022-09-23 11:21:11 +0200

    Initial commit

---
+++ MultiplesOf3or5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplesOf3or5
+{
+    internal class Program
+    {
+        public static int sumTotal = 0;
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 1000; i++)
+            {
+                
+                if ( (i % 3 == 0) || (i % 5 == 0) )
+                {
+                    Console.WriteLine(i);
+                    sumTotal += i;
+                }
+            }
+            Console.WriteLine(sumTotal);
+        }
+    }
+}

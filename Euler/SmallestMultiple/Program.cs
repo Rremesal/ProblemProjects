8b6bc19a6a5a186778ae8978e99a97794d308f60
Rremesal <raul.remesal@gmail.com>
2022-09-26 14:49:56 +0200

    created a new excercise (project)

---
+++ Euler/SmallestMultiple/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallestMultiple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 2520; i++)
+            {
+                for (int j = 1; j <= 20; j++)
+                {
+                    
+                    if( i % j == 0)
+                    {
+                        Console.WriteLine($"{i} / {j} = {i % j}");
+                    }
+                }
+            }
+                
+            
+        }
+    }
+}

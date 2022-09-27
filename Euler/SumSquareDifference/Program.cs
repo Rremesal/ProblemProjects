34623abfff9b577df0bf125168d6a80524767e40
Rremesal <raul.remesal@gmail.com>
2022-09-27 11:02:10 +0200

    solved the problem

---
+++ Euler/SumSquareDifference/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumSquareDifference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sumOfTheSquares = 0;
+            for(int i = 1; i <= 100; i++)
+            {
+                double square = Math.Pow(i, 2);
+                sumOfTheSquares += square;
+            }
+            Console.WriteLine($"The sum of the square is: {sumOfTheSquares}");
+
+            double sum = 0;
+            for(int j = 1; j <= 100; j++)
+            {
+                sum += j;
+            }
+            double squareOfTheSum = Math.Pow(sum, 2);
+            Console.WriteLine($"The square of the sum: {squareOfTheSum}");
+            double difference = squareOfTheSum - sumOfTheSquares;
+            Console.WriteLine($"The difference between the two: {difference}");
+
+        }
+    }
+}
---
+++ Rosetta Code/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<bool> listOfDoors = new List<bool>();
+        //false equals closed door, true equals open door
+        for(int i = 0; i < 100; i++) listOfDoors.Add(false);
+
+        //go past every door and open it
+        for(int i = 0; i < listOfDoors.Count(); i++)
+        {
+            Console.WriteLine($"{i} : {listOfDoors[i]}");
+        }
+
+
+
+    }
+}

4d58ee5c11c891b06cacc97ae8257d9995562409
Rremesal <raul.remesal@gmail.com>
2022-09-27 14:00:23 +0200

    took another attempt at solving problem

---
+++ Euler/10001stPrime/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10001stPrime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 2; //1 is no prime number
+            
+            for(int i = 1; i <= 6;i++)
+            {
+                int counter = 0;
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.WriteLine($"{i} : {value} --> divided by {j}");
+                    
+                    if (value % j == 0)
+                    {
+                        counter++;
+                    }
+                    if(counter == 2)
+                    {
+                        Console.WriteLine($"{i} : {value} is a prime number");
+                    }
+                    counter = 0;
+                }
+                
+                value++;
+            }
+
+            
+            
+        }
+
+    }
+}
